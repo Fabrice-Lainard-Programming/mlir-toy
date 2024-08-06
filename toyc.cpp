4772e88dd26d58a7cc96a1fbc39193d85f599da5
Fabrice Lainard Programming <59956392+Fabrice-Lainard-Programming@users.noreply.github.com>
2024-08-06 17:07:47 +0200

    feat: add llvm pass

--- toyc.cpp
+++ toyc.cpp
@@ -137,6 +137,8 @@ int loadMLIR(llvm::SourceMgr &sourceMgr, mlir::MLIRContext &context,
   return 0;
 }
 
+int dumpLLVMIR(mlir::ModuleOp module) ;
+
 int dumpMLIR()
 {
 mlir::DialectRegistry registry;
@@ -158,7 +160,7 @@ mlir::DialectRegistry registry;
     return 4;
 
    bool isLoweringToAffine = true;
-  //bool isLoweringToLLVM = false;
+  bool isLoweringToLLVM = true;
 
   if (enableOpt || isLoweringToAffine) {
     // Inline all functions into main and then delete them.
@@ -188,60 +190,7 @@ mlir::DialectRegistry registry;
     }
   }
 
-  if (mlir::failed(pm.run(*module)))
-    return 4;
-
-  module->dump();
-  return 0;
-  /*
-
-  mlir::MLIRContext context;
-  // Load our Dialect in this MLIR Context.
-  context.getOrLoadDialect<mlir::toy::ToyDialect>();
-
-  mlir::OwningOpRef<mlir::ModuleOp> module;
-  llvm::SourceMgr sourceMgr;
-  mlir::SourceMgrDiagnosticHandler sourceMgrHandler(sourceMgr, &context);
-  if (int error = loadMLIR(sourceMgr, context, module))
-    return error;
-  mlir::PassManager pm(module.get()->getName());
-
-  bool isLoweringToAffine = true;
-  bool isLoweringToLLVM = true;
-
-  if (enableOpt || isLoweringToAffine)
-  {
-    // Inline all functions into main and then delete them.
-    pm.addPass(mlir::createInlinerPass());
-
-    // Now that there is only one function, we can infer the shapes of each of
-    // the operations.
-    mlir::OpPassManager &optPM = pm.nest<mlir::toy::FuncOp>();
-    optPM.addPass(mlir::toy::createShapeInferencePass());
-    optPM.addPass(mlir::createCanonicalizerPass());
-    optPM.addPass(mlir::createCSEPass());
-  }
-
-  if (isLoweringToAffine)
-  {
-    // Partially lower the toy dialect.
-    pm.addPass(mlir::toy::createLowerToAffinePass());
-
-    // Add a few cleanups post lowering.
-    mlir::OpPassManager &optPM = pm.nest<mlir::func::FuncOp>();
-    optPM.addPass(mlir::createCanonicalizerPass());
-    optPM.addPass(mlir::createCSEPass());
-
-    // Add optimizations if enabled.
-    if (enableOpt)
-    {
-      optPM.addPass(mlir::affine::createLoopFusionPass());
-      optPM.addPass(mlir::affine::createAffineScalarReplacementPass());
-    }
-  }
-
-  if (isLoweringToLLVM)
-  {
+    if (isLoweringToLLVM) {
     // Finish lowering the toy IR to the LLVM dialect.
     pm.addPass(mlir::toy::createLowerToLLVMPass());
     // This is necessary to have line tables emitted and basic
@@ -249,10 +198,14 @@ mlir::DialectRegistry registry;
     // emission directly from our frontend.
     pm.addPass(mlir::LLVM::createDIScopeForLLVMFuncOpPass());
   }
+
   if (mlir::failed(pm.run(*module)))
     return 4;
-  // module->dump();
-  return 0;*/
+
+  //module->dump();
+  dumpLLVMIR(*module);
+  return 0;
+  
 }
 
 int dumpAST()
@@ -270,6 +223,85 @@ int dumpAST()
   dump(*moduleAST);
   return 0;
 }
+
+
+int dumpLLVMIR(mlir::ModuleOp module) {
+  // Register the translation to LLVM IR with the MLIR context.
+  mlir::registerBuiltinDialectTranslation(*module->getContext());
+  mlir::registerLLVMDialectTranslation(*module->getContext());
+
+  // Convert the module to LLVM IR in a new LLVM IR context.
+  llvm::LLVMContext llvmContext;
+  auto llvmModule = mlir::translateModuleToLLVMIR(module, llvmContext);
+  if (!llvmModule) {
+    llvm::errs() << "Failed to emit LLVM IR\n";
+    return -1;
+  }
+
+  // Initialize LLVM targets.
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+
+  // Configure the LLVM Module
+  auto tmBuilderOrError = llvm::orc::JITTargetMachineBuilder::detectHost();
+  if (!tmBuilderOrError) {
+    llvm::errs() << "Could not create JITTargetMachineBuilder\n";
+    return -1;
+  }
+
+  auto tmOrError = tmBuilderOrError->createTargetMachine();
+  if (!tmOrError) {
+    llvm::errs() << "Could not create TargetMachine\n";
+    return -1;
+  }
+  mlir::ExecutionEngine::setupTargetTripleAndDataLayout(llvmModule.get(),
+                                                        tmOrError.get().get());
+
+  /// Optionally run an optimization pipeline over the llvm module.
+  auto optPipeline = mlir::makeOptimizingTransformer(
+      /*optLevel=*/enableOpt ? 3 : 0, /*sizeLevel=*/0,
+      /*targetMachine=*/nullptr);
+  if (auto err = optPipeline(llvmModule.get())) {
+    llvm::errs() << "Failed to optimize LLVM IR " << err << "\n";
+    return -1;
+  }
+  llvm::errs() << *llvmModule << "\n";
+  return 0;
+}
+
+int runJit(mlir::ModuleOp module) {
+  // Initialize LLVM targets.
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+
+  // Register the translation from MLIR to LLVM IR, which must happen before we
+  // can JIT-compile.
+  mlir::registerBuiltinDialectTranslation(*module->getContext());
+  mlir::registerLLVMDialectTranslation(*module->getContext());
+
+  // An optimization pipeline to use within the execution engine.
+  auto optPipeline = mlir::makeOptimizingTransformer(
+      /*optLevel=*/enableOpt ? 3 : 0, /*sizeLevel=*/0,
+      /*targetMachine=*/nullptr);
+
+  // Create an MLIR execution engine. The execution engine eagerly JIT-compiles
+  // the module.
+  mlir::ExecutionEngineOptions engineOptions;
+  engineOptions.transformer = optPipeline;
+  auto maybeEngine = mlir::ExecutionEngine::create(module, engineOptions);
+  assert(maybeEngine && "failed to construct an execution engine");
+  auto &engine = maybeEngine.get();
+
+  // Invoke the JIT-compiled function.
+  auto invocationResult = engine->invokePacked("main");
+  if (invocationResult) {
+    llvm::errs() << "JIT invocation failed\n";
+    return -1;
+  }
+
+  return 0;
+}
+
 
 int main(int argc, char **argv)
 {
